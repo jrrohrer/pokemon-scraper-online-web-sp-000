063f1b3a3125864f3637963652994de451321902
Jessica Rohrer <jessie.rohrer@gmail.com>
2020-12-30 21:12:00 +0000

    Automatically backed up by Learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -9,7 +9,7 @@ class Pokemon
   end
   
   def self.save(name, type, db)
-    sql = "INSERT INTO pokemon VALUES (?, ?)"
+    sql = "INSERT INTO pokemon (name, type) VALUES (?, ?)"
     db.execute(sql, self.name, self.type)
   end 
 end
