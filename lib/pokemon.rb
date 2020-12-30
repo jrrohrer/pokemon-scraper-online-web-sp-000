ecdeafb1caa3a4e9644fb2fc8e49805bb24ec15e
Jessica Rohrer <jessie.rohrer@gmail.com>
2020-12-30 21:27:33 +0000

    Automatically backed up by Learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -15,6 +15,7 @@ class Pokemon
   def self.find(id, db)
     #find pokemon by id
     #return a new pokemon object
-    db.execute("SELECT * FROM pokemon WHERE id = ?", id)
+    new_pokemon = db.execute("SELECT * FROM pokemon WHERE id = ?", id).flatten
+    Pokemon.new(id: new_pokemon[0], name: new_pokemon[1], type: new_pokemon[2], db: db)
   end
 end
